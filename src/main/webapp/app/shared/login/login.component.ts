351989064c86810a01f81b19c248bd326a884d45
duheng <heng.du@qq.com>
2018-07-16 16:38:02 +0800

    增加匿名登录按钮

--- src/main/webapp/app/shared/login/login.component.ts
+++ src/main/webapp/app/shared/login/login.component.ts
@@ -71,6 +71,35 @@ export class JhiLoginModalComponent implements AfterViewInit {
             this.authenticationError = true;
         });
     }
+    
+	nonameLogin() {
+        this.loginService.login({
+            username: 'guest',
+            password: 'guest',
+            rememberMe: this.rememberMe
+        }).then(() => {
+            this.authenticationError = false;
+            this.activeModal.dismiss('login success');
+            if (this.router.url === '/register' || (/activate/.test(this.router.url)) ||
+                this.router.url === '/finishReset' || this.router.url === '/requestReset') {
+                this.router.navigate(['']);
+            }
+
+            this.eventManager.broadcast({
+                name: 'authenticationSuccess',
+                content: 'Sending Authentication Success'
+            });
+
+            // // previousState was set in the authExpiredInterceptor before being redirected to login modal.
+            // // since login is succesful, go to stored previousState and clear previousState
+            const redirect = this.stateStorageService.getUrl();
+            if (redirect) {
+                this.router.navigate([redirect]);
+            }
+        }).catch(() => {
+            this.authenticationError = true;
+        });
+    }
 
     register() {
         this.activeModal.dismiss('to state register');
